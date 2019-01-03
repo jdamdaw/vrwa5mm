fee2be30e0bc9c199093633f826eb971218b1c0d
jesus <jdamdaw@gmail.com>
2019-01-03 01:20:23 +0000

    c5 - defino la clase con su constructor

--- src/app/libro/libro.component.ts
+++ src/app/libro/libro.component.ts
@@ -7,12 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class LibroComponent{
+    /**
+     * VOY A HACERLO BIEN, SU CONSTRUCTOR
     public nombreLibro="me echaron droja en el cola cao";
     public descripcion="uno que salía en un vídeo";
     public paraAdultos=true;
     public colorParaAdultos="red";
     public colores=["red","lime","purple"];
-    //public aleatorio=0;
+    */
+    //propiedades o atributos
+    public nombreLibro:string;
+    public descripcion:string;
+    //public paraAdultos:boolean;   //no la uso
+    public colores:Array<string>;
+    //constructor
+    constructor(){
+        this.nombreLibro="me echaron droja en el cola cao";
+        this.descripcion="uno que salía en un vídeo";
+        this. colores=["red","lime","purple"];
+    }
 
     public colorAleatorio(){
     //    return Math.floor(Math.random()*this.colores.length);
